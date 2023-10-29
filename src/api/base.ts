042513655722890b373fcbdad7656bf376653c06
tangchungjiao <2074623967@qq.com>
2023-10-29 11:11:33 +0800

    feat: 商家详情页组件ShopItem开发

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,8 +1,17 @@
 import axios from 'axios'
 import { showDialog } from 'vant'
+import { useLocalStorage } from '@/use/useLocalStorage'
 
 const instance = axios.create({
   baseURL: '/api',
+})
+
+instance.interceptors.request.use((config) => {
+  const { value: token } = useLocalStorage('token', '')
+  if (config.headers && token.value) {
+    config.headers['x-token'] = token.value
+  }
+  return config
 })
 
 instance.interceptors.response.use(
